a4715cb554327c639348a0bb2c925a7352811ba9
frohikey <frohikey@gmail.com>
2023-07-11 14:58:34 +0200

    Phone props are public

--- Oogi2.AspNetCore3.Identity/IdentityUser.cs
+++ Oogi2.AspNetCore3.Identity/IdentityUser.cs
@@ -17,15 +17,14 @@ namespace Oogi2.AspNetCore3.Identity
     /// Represents a user in the identity system for the <see cref="Stores.DocumentDbUserStore{TUser, TRole}"/>
     /// </summary>
     public class IdentityUser<TRole> : BaseEntity
-    {
-        public string Id { get; set; }
+    {        
         public string UserName { get; set; }
         public string Email { get; set; }
         public string NormalizedUserName { get; set; }
         public string NormalizedEmail { get; set; }
         public bool IsEmailConfirmed { get; set; }
-        public string PhoneNumber { get; internal set; }
-        public bool IsPhoneNumberConfirmed { get; internal set; }
+        public string PhoneNumber { get; set; }
+        public bool IsPhoneNumberConfirmed { get; set; }
         public string PasswordHash { get; set; }
         public string SecurityStamp { get; set; }
         public bool IsTwoFactorAuthEnabled { get; set; }
